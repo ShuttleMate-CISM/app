05853cba5aaf4111b32367dedffc65628caa7310
mudunkotuwasb <110487612+mudunkotuwasb@users.noreply.github.com>
2026-02-17 21:53:08 +0530

    CSP: style-src unsafe stx inline - Remove Inline Styles with with CSS classes. Added CSP headers to backend (server.js).

--- Shuttlemate-Backend/server.js
+++ Shuttlemate-Backend/server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
     [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://www.gstatic.com",
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+      "style-src 'self' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com data:",
       "img-src 'self' data: https: blob:",
       "connect-src 'self' http://localhost:5001 https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com",
